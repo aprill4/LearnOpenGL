e958dc7856ac0e52194f306bbae29b54c1c3847a
aprill4 <1545752334@qq.com>
2024-11-10 14:10:14 +0800

    set up

---
+++ hello_triangle.cpp
@@ -0,0 +1,8 @@
+// $ clang++ -o # @ -I -std=c++17 -stdlib=libc++ -lglfw -framework CoreVideo -framework OpenGL -framework IOKit -framework Cocoa -framework Carbon
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+  return 0;
+}
